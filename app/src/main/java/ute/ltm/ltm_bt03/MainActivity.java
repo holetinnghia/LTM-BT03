3e25d592e9f3504959c027ebbf387553a4e9e8a5
holetinnghia <holetinnghia@icloud.com>
2025-11-17 19:52:17 +0700

    Recycler View Basic

--- app/src/main/java/ute/ltm/ltm_bt03/MainActivity.java
+++ app/src/main/java/ute/ltm/ltm_bt03/MainActivity.java
@@ -51,5 +51,14 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        Button btnRecyclerViewBasic = findViewById(R.id.btnRecyclerViewBasic);
+        btnRecyclerViewBasic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ute.ltm.ltm_bt03.recyclerview.RecyclerViewBasicActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
